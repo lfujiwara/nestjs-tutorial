eb0aba39e1e4ed4a427b04ab8a46268fcf1f8537
Lucas <lucastfujiwara@gmail.com>
2021-08-01 00:49:50 -0300

    chore: setup auth module add auth controller, include register-new-user-service and its deps

--- src/auth/auth.module.ts
+++ src/auth/auth.module.ts
@@ -1,4 +1,30 @@
+import { AuthController } from './auth.controller';
+import { DataModule } from '../data/data.module';
 import { Module } from '@nestjs/common';
+import { RegisterNewUserService } from './services/register-new-user/register-new-user.service';
+import { UserRepository } from '../data/repositories/user.repository';
 
-@Module({})
+@Module({
+  imports: [DataModule],
+  providers: [
+    RegisterNewUserService,
+    {
+      provide: 'RegisterNewUserService.emailIsAlreadyRegistered',
+      useFactory: (repo: UserRepository) => repo.isEmailRegistered.bind(repo),
+      inject: [UserRepository],
+    },
+    {
+      provide: 'RegisterNewUserService.usernameIsAlreadyRegistered',
+      useFactory: (repo: UserRepository) =>
+        repo.isUsernameRegistered.bind(repo),
+      inject: [UserRepository],
+    },
+    {
+      provide: 'RegisterNewUserService.registerNewUser',
+      useFactory: (repo: UserRepository) => repo.registerNewUser.bind(repo),
+      inject: [UserRepository],
+    },
+  ],
+  controllers: [AuthController],
+})
 export class AuthModule {}
