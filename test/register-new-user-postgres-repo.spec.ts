eed85f165ff726432c63fe0dc9004c0019d9e3a9
Lucas <lucastfujiwara@gmail.com>
2021-08-01 00:28:20 -0300

    chore: integration tests register-new-user + postgres user repository

---
+++ test/register-new-user-postgres-repo.spec.ts
@@ -0,0 +1,102 @@
+import {
+  RegisterNewUserService,
+  RegisterNewUserServiceErrors,
+} from '../src/auth/services/register-new-user/register-new-user.service';
+import { Test, TestingModule } from '@nestjs/testing';
+
+import { ConfigModule } from '@nestjs/config';
+import { Pool } from 'pg';
+import { UserRepository } from '../src/data/repositories/user.repository';
+
+describe('RegisterNewUserService + UserRepository', () => {
+  let service: RegisterNewUserService;
+  let module: TestingModule;
+
+  beforeEach(async () => {
+    module = await Test.createTestingModule({
+      imports: [
+        ConfigModule.forRoot({
+          envFilePath: ['.env.test'],
+        }),
+      ],
+      providers: [
+        Pool,
+        UserRepository,
+        {
+          provide: 'RegisterNewUserService.emailIsAlreadyRegistered',
+          useFactory: (repo: UserRepository) =>
+            repo.isEmailRegistered.bind(repo),
+          inject: [UserRepository],
+        },
+        {
+          provide: 'RegisterNewUserService.usernameIsAlreadyRegistered',
+          useFactory: (repo: UserRepository) =>
+            repo.isUsernameRegistered.bind(repo),
+          inject: [UserRepository],
+        },
+        {
+          provide: 'RegisterNewUserService.registerNewUser',
+          useFactory: (repo: UserRepository) => repo.registerNewUser.bind(repo),
+          inject: [UserRepository],
+        },
+        RegisterNewUserService,
+      ],
+    }).compile();
+
+    service = module.get<RegisterNewUserService>(RegisterNewUserService);
+  });
+
+  afterEach(async () => {
+    const pool = module.get<Pool>(Pool);
+    pool?.query('DELETE FROM users;');
+  });
+
+  afterAll(async () => {
+    const pool = module.get<Pool>(Pool);
+    pool?.end();
+  });
+
+  it('should be defined', () => {
+    expect(service).toBeDefined();
+  });
+
+  const validInput = {
+    username: 'johndoe',
+    email: 'johndoe@gmail.com',
+    password: 'johndoe123',
+  };
+
+  it('should execute normally with a valid input', async () => {
+    await expect(service.execute({ ...validInput })).resolves.toBeTruthy();
+  });
+
+  it('should throw an error if the email is already registered', async () => {
+    await service.execute({
+      ...validInput,
+      username: 'johndoe2',
+    });
+
+    await expect(
+      service.execute({
+        ...validInput,
+      }),
+    ).rejects.toThrowError(
+      RegisterNewUserServiceErrors.EMAIL_ALREADY_REGISTERED,
+    );
+  });
+
+  it('should throw an error if the username is already registered', async () => {
+    await service.execute({
+      ...validInput,
+      email: 'johndoe2@gmail.com',
+    });
+
+    await expect(
+      service.execute({
+        ...validInput,
+      }),
+    ).rejects.toThrowError(
+      RegisterNewUserServiceErrors.USERNAME_ALREADY_REGISTERED,
+    );
+  });
+});
